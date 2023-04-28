e4d5faea9657e3f639ddecce2c5a71275874f797
Maksim Lin <maks@manichord.com>
2023-04-28 11:50:44 +1000

    minimal w/ sashimi rotation control using keys

---
+++ lib/game.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:flame/components.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sashimi/sashimi.dart';
+
+class ExampleGame extends SashimiGame with KeyboardEvents, MultiTouchDragDetector {
+  Set<LogicalKeyboardKey> _keysPressed = {};
+
+  @override
+  Future<void> onLoad() async {
+    final position = Vector3(10, 10, 0);
+    const double scale = 10;
+
+    final model = Model(
+      position: position,
+      sliceSize: Vector2.all(16),
+      size: Vector3.all(16),
+      scale: Vector3(scale, scale, 1),
+      angle: 45 * degrees2Radians,
+      image: await images.load('maksBlueCar.png'),
+    );
+
+    await add(model);
+
+    kamera
+      ..follow(PositionComponent())
+      ..viewfinder.zoom = 1;
+
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    final rotateLeft = _keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final rotateRight = _keysPressed.contains(LogicalKeyboardKey.arrowRight);
+    final rotation = rotateLeft ? 1 : (rotateRight ? -1 : 0);
+    kamera.rotation += rotation * dt;
+
+    super.update(dt);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    _keysPressed = keysPressed;
+    return KeyEventResult.handled;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+
+import 'game.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late final ExampleGame _game;
+
+  @override
+  void initState() {
+    super.initState();
+    _game = ExampleGame();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: GameWidget(game: _game),
         ),
       ),
     );
